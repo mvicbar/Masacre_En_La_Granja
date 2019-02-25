1f7d6b4faea9234c7d05d97c6440c2bb3ed2bc01
mvicbar <mariavba@ucm.es>
2019-02-25 18:54:13 +0100

    Proyecto creado de nuevo; añadido el leeme.html

---
+++ Masacre_en_la_granja/src/main/java/es/ucm/fdi/iw/MasacreEnLaGranjaApplication.java
@@ -0,0 +1,13 @@
+package es.ucm.fdi.iw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MasacreEnLaGranjaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MasacreEnLaGranjaApplication.class, args);
+	}
+
+}
