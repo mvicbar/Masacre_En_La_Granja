23594d17323bc167f7cae72d9d1fe0bdda83bb42
mvicbar <mariavba@ucm.es>
2019-03-04 18:19:25 +0100

    Actualizado el fichero .sql de la BD según la imagen

--- Masacre_en_la_Granja/src/main/resources/masacre.sql
+++ Masacre_en_la_Granja/src/main/resources/masacre.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 25-02-2019 a las 19:42:58
+-- Tiempo de generación: 04-03-2019 a las 18:15:59
 -- Versión del servidor: 10.1.36-MariaDB
 -- Versión de PHP: 7.2.11
 
@@ -25,36 +25,26 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `deaths`
---
-
-CREATE TABLE `deaths` (
-  `id_killer` int(11) UNSIGNED NOT NULL,
-  `id_victim` int(11) UNSIGNED NOT NULL,
-  `num_deaths` int(11) UNSIGNED NOT NULL DEFAULT '1'
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
-
--- --------------------------------------------------------
-
---
 -- Estructura de tabla para la tabla `game`
 --
 
 CREATE TABLE `game` (
   `id` int(11) UNSIGNED NOT NULL,
-  `creation_time` datetime NOT NULL
+  `creation_time` datetime NOT NULL,
+  `status` varchar(5000) COLLATE utf8_spanish_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
 
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `status`
+-- Estructura de tabla para la tabla `stat`
 --
 
-CREATE TABLE `status` (
-  `id_game` int(11) UNSIGNED NOT NULL,
-  `status_order` int(11) NOT NULL,
-  `data` varchar(1000) COLLATE utf8_spanish_ci NOT NULL
+CREATE TABLE `stat` (
+  `id` int(11) UNSIGNED NOT NULL,
+  `name` varchar(100) COLLATE utf8_spanish_ci NOT NULL,
+  `description` varchar(100) COLLATE utf8_spanish_ci NOT NULL,
+  `code` varchar(5000) COLLATE utf8_spanish_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
 
 -- --------------------------------------------------------
@@ -66,33 +56,7 @@ CREATE TABLE `status` (
 CREATE TABLE `user` (
   `id` int(11) UNSIGNED NOT NULL,
   `name` varchar(100) COLLATE utf8_spanish_ci NOT NULL,
-  `password` varchar(100) COLLATE utf8_spanish_ci NOT NULL,
-  `role` varchar(100) COLLATE utf8_spanish_ci NOT NULL,
-  `victories` int(11) UNSIGNED NOT NULL DEFAULT '0',
-  `defeats` int(11) UNSIGNED NOT NULL DEFAULT '0'
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `user_achievements`
---
-
-CREATE TABLE `user_achievements` (
-  `id_user` int(11) UNSIGNED NOT NULL,
-  `first_game` tinyint(1) UNSIGNED NOT NULL DEFAULT '0',
-  `devoured` tinyint(1) UNSIGNED NOT NULL DEFAULT '0',
-  `survive` tinyint(1) UNSIGNED NOT NULL DEFAULT '0',
-  `10_games` tinyint(1) UNSIGNED NOT NULL DEFAULT '0',
-  `100_games` tinyint(1) UNSIGNED NOT NULL DEFAULT '0',
-  `draw` tinyint(1) UNSIGNED NOT NULL DEFAULT '0',
-  `win_as_vampire` tinyint(1) UNSIGNED NOT NULL DEFAULT '0',
-  `win_as_human` tinyint(1) UNSIGNED NOT NULL DEFAULT '0',
-  `majors_murder` tinyint(1) UNSIGNED NOT NULL DEFAULT '0',
-  `loved` tinyint(1) UNSIGNED NOT NULL DEFAULT '0',
-  `boring_night` tinyint(1) UNSIGNED NOT NULL DEFAULT '0',
-  `fool_discovered` tinyint(1) UNSIGNED NOT NULL DEFAULT '0',
-  `kill_the_old_man` tinyint(1) UNSIGNED NOT NULL DEFAULT '0'
+  `password` varchar(100) COLLATE utf8_spanish_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
 
 -- --------------------------------------------------------
@@ -109,37 +73,18 @@ CREATE TABLE `user_game` (
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `user_roles`
+-- Estructura de tabla para la tabla `user_stat`
 --
 
-CREATE TABLE `user_roles` (
+CREATE TABLE `user_stat` (
   `id_user` int(11) UNSIGNED NOT NULL,
-  `vampiro` int(11) UNSIGNED NOT NULL DEFAULT '0',
-  `conde` int(11) UNSIGNED NOT NULL DEFAULT '0',
-  `humano` int(11) UNSIGNED NOT NULL DEFAULT '0',
-  `adivina` int(11) UNSIGNED NOT NULL DEFAULT '0',
-  `cazavampiros` int(11) UNSIGNED NOT NULL DEFAULT '0',
-  `alquimista` int(11) UNSIGNED NOT NULL DEFAULT '0',
-  `picaro` int(11) UNSIGNED NOT NULL DEFAULT '0',
-  `niña` int(11) UNSIGNED NOT NULL DEFAULT '0',
-  `alcalde` int(11) UNSIGNED NOT NULL DEFAULT '0',
-  `dama_noche` int(11) UNSIGNED NOT NULL DEFAULT '0',
-  `borracho` int(11) UNSIGNED NOT NULL DEFAULT '0',
-  `tonto` int(11) UNSIGNED NOT NULL DEFAULT '0',
-  `anciano` int(11) UNSIGNED NOT NULL DEFAULT '0'
+  `id_stat` int(11) UNSIGNED NOT NULL,
+  `status` int(11) UNSIGNED NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
 
 --
 -- Índices para tablas volcadas
 --
-
---
--- Indices de la tabla `deaths`
---
-ALTER TABLE `deaths`
-  ADD UNIQUE KEY `unique_user_murder` (`id_killer`,`id_victim`),
-  ADD KEY `id_killer` (`id_killer`),
-  ADD KEY `id_murdered` (`id_victim`);
 
 --
 -- Indices de la tabla `game`
@@ -148,11 +93,10 @@ ALTER TABLE `game`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indices de la tabla `status`
+-- Indices de la tabla `stat`
 --
-ALTER TABLE `status`
-  ADD UNIQUE KEY `unique_status_game` (`id_game`),
-  ADD KEY `id_game` (`id_game`);
+ALTER TABLE `stat`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indices de la tabla `user`
@@ -160,12 +104,6 @@ ALTER TABLE `status`
 ALTER TABLE `user`
   ADD PRIMARY KEY (`id`),
   ADD UNIQUE KEY `name` (`name`);
-
---
--- Indices de la tabla `user_achievements`
---
-ALTER TABLE `user_achievements`
-  ADD PRIMARY KEY (`id_user`);
 
 --
 -- Indices de la tabla `user_game`
@@ -176,10 +114,12 @@ ALTER TABLE `user_game`
   ADD KEY `id_game` (`id_game`);
 
 --
--- Indices de la tabla `user_roles`
+-- Indices de la tabla `user_stat`
 --
-ALTER TABLE `user_roles`
-  ADD PRIMARY KEY (`id_user`);
+ALTER TABLE `user_stat`
+  ADD UNIQUE KEY `unique_user_stat` (`id_user`,`id_stat`),
+  ADD KEY `id_user` (`id_user`),
+  ADD KEY `id_stat` (`id_stat`);
 
 --
 -- AUTO_INCREMENT de las tablas volcadas
@@ -189,6 +129,12 @@ ALTER TABLE `user_roles`
 -- AUTO_INCREMENT de la tabla `game`
 --
 ALTER TABLE `game`
+  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `stat`
+--
+ALTER TABLE `stat`
   MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT;
 
 --
@@ -202,25 +148,6 @@ ALTER TABLE `user`
 --
 
 --
--- Filtros para la tabla `deaths`
---
-ALTER TABLE `deaths`
-  ADD CONSTRAINT `id_killer_fk` FOREIGN KEY (`id_killer`) REFERENCES `user` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `id_murdered_fk` FOREIGN KEY (`id_victim`) REFERENCES `user` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
-
---
--- Filtros para la tabla `status`
---
-ALTER TABLE `status`
-  ADD CONSTRAINT `id_game_fk` FOREIGN KEY (`id_game`) REFERENCES `game` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
-
---
--- Filtros para la tabla `user_achievements`
---
-ALTER TABLE `user_achievements`
-  ADD CONSTRAINT `id_user_achievements_fk` FOREIGN KEY (`id_user`) REFERENCES `user` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
-
---
 -- Filtros para la tabla `user_game`
 --
 ALTER TABLE `user_game`
@@ -228,10 +155,11 @@ ALTER TABLE `user_game`
   ADD CONSTRAINT `id_user_fk_user_game` FOREIGN KEY (`id_user`) REFERENCES `user` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
--- Filtros para la tabla `user_roles`
+-- Filtros para la tabla `user_stat`
 --
-ALTER TABLE `user_roles`
-  ADD CONSTRAINT `id_user_fk` FOREIGN KEY (`id_user`) REFERENCES `user` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE `user_stat`
+  ADD CONSTRAINT `id_stat_fk_user_stat` FOREIGN KEY (`id_stat`) REFERENCES `stat` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `id_user_fk_user_stat` FOREIGN KEY (`id_user`) REFERENCES `user` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
