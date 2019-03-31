3191e706ce131e302c741a68c747176cadf7d05f
Char-Mander <laurajf29@gmail.com>
2019-03-31 18:20:17 +0200

    puesto el atributo de sesión a null en el logout

--- src/main/java/es/ucm/fdi/iw/control/UserController.java
+++ src/main/java/es/ucm/fdi/iw/control/UserController.java
@@ -218,6 +218,7 @@ public class UserController {
 	public String logout(Model model, HttpSession session) {
 		//	¿Borrar el atributo "user" de sesión? En ese caso, en la función 'register'
 		//	y 'login' habría que añadirlo, no cambiar su valor
+		session.setAttribute("user", null);
 		return "redirect:/user/login";
 	}
 
