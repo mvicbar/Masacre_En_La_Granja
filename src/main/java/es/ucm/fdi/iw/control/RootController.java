be23600b07912668b19bbeee9cc87f69b8b264f0
Char-Mander <36884991+Char-Mander@users.noreply.github.com>
2019-04-03 12:38:20 +0200

    Pruebas para el template de partida
    En qué momento crearon la hoja de tomillo

--- src/main/java/es/ucm/fdi/iw/control/RootController.java
+++ src/main/java/es/ucm/fdi/iw/control/RootController.java
@@ -1,10 +1,15 @@
 package es.ucm.fdi.iw.control;
 
 import java.security.Principal;
+import java.util.ArrayList;
+import java.util.List;
 
+import javax.persistence.EntityManager;
 import javax.servlet.ServletContext;
 import javax.servlet.http.HttpServletRequest;
 
+import es.ucm.fdi.iw.model.User;
+import es.ucm.fdi.iw.model.UserStat;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -21,13 +26,29 @@ public class RootController {
 	
 	@Autowired
 	private Environment env;
+
+	@Autowired
+	private EntityManager entityManager;
 	
 	@Autowired
 	private IwSocketHandler iwSocketHandler;
 
 	@GetMapping("/")
 	public String index(Model model) {
-		return "inicio";
+		Integer n1 = 1, n2 = 2;
+
+		User u1 = entityManager.find(User.class, n1.longValue());
+		User u2 = entityManager.find(User.class, n2.longValue());
+		UserStat us1 = new UserStat();
+		UserStat us2 = new UserStat();
+		us1.setUser(u1);
+		us2.setUser(u2);
+		List<UserStat> all = new ArrayList<>();
+		all.add(us1);
+		all.add(us2);
+		model.addAttribute("userStats", all);
+
+		return "partida";
 	}
 	
 	@GetMapping("/reglas")
