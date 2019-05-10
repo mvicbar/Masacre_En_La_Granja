17dee4b82bacfd24730e55a4fd61e14c2d38360c
ProConrad <ppdomenecha@gmail.com>
2019-05-10 13:07:55 +0200

    Preparar codigo para empezar con el chat

--- src/main/resources/static/js/kclient.js
+++ src/main/resources/static/js/kclient.js
@@ -3,13 +3,23 @@ function addNewPlayerToLobby(name) {
 	miembros[0].innerHTML = miembros[0].innerHTML + "<td>" + name + "</td>";
 }
 
+function receiveChatMessage(message) {
+	const lineOutput = document.getElementById("recibido");
+	lineOutput.value = lineOutput.value + '\n' + message;
+}
+
 const handleNewPlayer = (name) => {
 	addNewPlayerToLobby(name);
+}
+
+const handleChatMessage = (chatMessage) => {
+	receiveChatMessage(chatMessage);
 }
 
 const handleMessage = (o) => {
 	console.log(o);
 	if (o.newPlayer) handleNewPlayer(o.newPlayer);
+	if (o.chatMessage) handleChatMessage(o.chatMessage);
 }
 
 window.addEventListener('load', () => {
@@ -26,4 +36,61 @@ window.addEventListener('load', () => {
 		}
 	}
 	window.setInterval(() => { }, 5000);
+});
+
+/**
+			 * WebSocket API, which only works once initialized
+			 */
+const ws = {
+
+	/**
+	 * WebSocket, or null if none connected
+	 */
+	socket: null,
+
+	/**
+	 * Sends a string to the server via the websocket.
+	 * @param {string} text to send 
+	 * @returns nothing
+	 */
+	send: (text) => {
+		if (ws.socket != null) {
+			ws.socket.send(text);
+		}
+	},
+
+	/**
+	 * Default action when text is received. 
+	 * @returns
+	 */
+	receive: (text) => {
+		console.log(text);
+		obj = JSON.parse(text);
+		handleMessage(obj);
+	},
+
+	/**
+	 * Attempts to establish communication with the specified
+	 * web-socket endpoint. If successfull, will call 
+	 * @returns
+	 */
+	initialize: (endpoint) => {
+		try {
+			ws.socket = new WebSocket(endpoint);
+			ws.socket.onmessage = (e) => ws.receive(e.data);
+			console.log("Connected to WS '" + endpoint + "'")
+		} catch (e) {
+			console.log("Error, connection to WS '" + endpoint + "' FAILED: ", e);
+		}
+	}
+}
+
+/**
+ * Actions to perform once the page is fully loaded
+ */
+window.addEventListener('load', () => {
+	if (config.socketUrl !== false) {
+		ws.initialize(config.socketUrl);
+	}
+
 });
